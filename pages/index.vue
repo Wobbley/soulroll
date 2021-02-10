697e93610a5b60ea69071c71c5da1572b0149a59
Dean Lozo <dean.lozo@gmail.com>
2021-02-10 21:15:23 +0100

    Started messing around

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,42 @@
 <template>
-  <div class="container">
+  <div class="flex justify-center space-x-4">
     <div>
-      <Logo />
-      <h1 class="title">soulroll</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <label class="block mb-2 text-indigo-500" for="name">Roller Name</label>
+      <input
+        class="p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
+        type="text"
+        name="username"
+      />
+    </div>
+    <div>
+      <div>
+        <label class="block mb-2 text-indigo-500" for="amount">How many dice?</label>
+        <input
+          class="p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
+          type="number"
+          name="username"
+        />
       </div>
+      <div class="block">
+        <label class="block mb-2 text-indigo-500" for="username">Dice Size?</label>
+        <input
+          class="p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
+          type="number"
+          name="username"
+        />
+      </div>
+    </div>
+    <div>
+      <label class="block mb-2 text-indigo-500" for="username">Sucess is</label>
+      <input
+        class="p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
+        type="number"
+        name="username"
+      />
+      <label class="block mb-2 text-indigo-500" for="username">and up </label>
+    </div>
+    <div>
+      <button class="bg-green-500">Button</button>
     </div>
   </div>
 </template>
@@ -29,40 +45,4 @@
 export default {}
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
